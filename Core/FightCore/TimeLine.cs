c571f2eef7e1d271f4ea3e61e1b71281996dd712
JookiTsui <781478448@qq.com>
2021-03-02 18:39:15 +0800

    增加时间线和计时器工具

---
+++ Core/FightCore/TimeLine.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// 主要用于控制播放动画的时间点及相应的函数响应
+/// </summary>
+public class TimeLine{
+    private Action<float> _update;
+    private Action _reset;
+    // 当前计时
+    private float _curTime = 0;
+    // 是否开始
+    private bool _isStart = false;
+    // 是否暂停
+    private bool _isPause = false;
+
+    public void Start()    {        Reset();        _isStart = true;        _isPause = false;    }
+
+    /// <summary>
+    /// 添加时间线
+    /// </summary>
+    /// <param name="time"></param>
+    /// <param name="ID"></param>
+    /// <param name="callBack"></param>
+    public void AddEvent(float time, int ID, Action<int> callBack)    {        LineEvent param = new LineEvent(time, ID, callBack);        _update += param.Invoke;        _reset += param.Reset;    }
+
+    /// <summary>
+    /// 时间线驱动，需要从外部的Update函数中驱动
+    /// </summary>
+    /// <param name="deltaTime"></param>
+    public void Loop(float deltaTime)    {        if (!_isStart || _isPause)        {            return;        }        _curTime += deltaTime;        if (_update != null)        {            _update(_curTime);        }    }
+
+    /// <summary>
+    /// 暂停或恢复时间线
+    /// </summary>
+    public void Pause()    {        _isPause = true;    }
+
+    /// <summary>
+    /// 恢复时间线
+    /// </summary>
+    public void Resume()    {        _isPause = false;    }
+
+    /// <summary>
+    /// 停止时间线
+    /// </summary>
+    public void Stop()    {        Reset();        _update = null;        _reset = null;    }
+
+    public void Reset()    {        _curTime = 0;        _isStart = false;        _isPause = false;        if(_reset != null)        {            _reset.Invoke();        }    }
+}
+
+public class LineEvent{
+    public float Delay; // 调用时间点
+    public int ID; // 调用的动画ID
+    public Action<int> Method; // 动画动作完成的响应
+
+    private bool _isInvoke = false;
+
+    public LineEvent(float time, int animID, Action<int> callback)    {
+        Delay = time;
+        ID = animID;
+        Method = callback;
+
+        Reset();
+    }
+
+    public void Invoke(float time)    {        if (time < Delay)        {            return;        }        if (!_isInvoke && Method != null)        {            Method(ID);            _isInvoke = true;        }    }
+
+    public void Reset()    {        _isInvoke = false;    }
+}
---
+++ Core/Timer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+/// <summary>
+/// 自制定时器
+/// </summary>
+public class TimerMgr : Singleton<TimerMgr>
+{
+    private Action<float> _loopCallbacks;
+    // 暂停所有计算器
+    private bool _isPause;
+    
+    /// <summary>
+    /// repeatTimes小于等于0，代表无限循环
+    /// </summary>
+    /// <param name="delayTime"></param>
+    /// <param name="repeatTimes"></param>
+    /// <param name="callback"></param>
+    /// <returns></returns>
+    public Timer CreateTimer(float delayTime, int repeatTimes, Action callback)    {        Timer timer = new Timer(delayTime, repeatTimes, callback);        _loopCallbacks += timer.Loop;        return timer;    }
+
+    public void Start(Timer timer)    {        timer.Start();    }
+
+    /// <summary>
+    /// repeatTimes小于等于0，代表无限循环
+    /// </summary>
+    /// <param name="delayTime"></param>
+    /// <param name="repeatTimes"></param>
+    /// <param name="callback"></param>
+    public void CreateTimerAndStart(float delayTime, Action callback, int repeatTimes = 1)    {        Start(CreateTimer(delayTime, repeatTimes, callback));    }
+
+    /// <summary>
+    /// Loop需要通过外部的MonoBehavior的Update来驱动
+    /// </summary>
+    /// <param name="deltaTime"></param>
+    public void Loop(float deltaTime)    {        if (_loopCallbacks != null && !_isPause)        {            _loopCallbacks.Invoke(deltaTime);        }    }
+
+    /// <summary>
+    /// 暂停或恢复所有计时器
+    /// </summary>
+    public void PauseAll()	{        _isPause = !_isPause;	}
+
+    /// <summary>
+    /// 停止所有计时器
+    /// </summary>
+    public void StopAll()	{        _loopCallbacks = null;	}
+
+    /// <summary>
+    /// 暂停指定的计时器
+    /// </summary>
+    /// <param name="timer"></param>
+    public void Pause(Timer timer)	{        timer.Pause();	}
+
+    /// <summary>
+    /// 停止指定的计时器
+    /// </summary>
+    /// <param name="timer"></param>
+    public void Stop(Timer timer)    {        Pause(timer);        if(_loopCallbacks != null)        {            _loopCallbacks -= timer.Loop;        }    }
+}
+
+public class Timer{
+    // 执行的间隔时间
+    private float _delayTime;
+    // 方法要重复的次数
+    private int _repeatTimes;
+    private Action _method;
+
+    // 是否开始计时
+    private bool _isStart = false;
+    // 是否暂停计时器
+    private bool _isPause = false;
+    // 当前持续的时间
+    private float _durationTime = 0;
+    // 已经重复的次数
+    private int _repeatedTimes = 0;
+
+    public Timer(float delayTime, int repeatTimes, Action callback)    {        _delayTime = delayTime;        _repeatTimes = repeatTimes;        _method = callback;    }
+
+    public void Start()	{        _isStart = true;	}
+
+    public void Loop(float detaTime)    {        if (_isStart && !_isPause)        {            _durationTime += detaTime;            if (_durationTime < _delayTime)            {                return;            }            _durationTime -= _delayTime;            _repeatedTimes++;            if (_method != null)            {                _method.Invoke();            }            if (_repeatTimes > 0 &&  _repeatedTimes >= _repeatTimes)            {                TimerMgr.Instance.Stop(this);            }        }    }
+
+    public void Pause()	{        _isPause = true;	}
+
+    public void Resume()	{        _isPause = false;	}
+}
+
